ca789b051f667a3c5a6146314adbc37543fe48da
deveduardoalbuquerque <luiz.barbosa@zup.com.br>
2021-06-28 15:49:53 -0300

    basic core structure

---
+++ src/main/kotlin/br/com/warrios/core/model/Sale.kt
@@ -0,0 +1,10 @@
+package br.com.warrios.core.model
+
+import io.micronaut.core.annotation.Introspected
+import java.util.*
+
+@Introspected
+data class Sale (
+    val productid: UUID,
+    val qty: Double
+        )
